af8cb3560c9b32283ce491babacaca8f955900ed
bacondrake <frank.pavey@gmail.com>
2017-06-14 21:19:43 +1000

    User model works with Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :lockable, :timeoutable and :omniauthable
   devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, length: { maximum: 255 },
+  #           format: { with: VALID_EMAIL_REGEX },
+  #           uniqueness: { case_sensitive: false }
+  # has_secure_password
 end
